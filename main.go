233eaceb1cbf7f5bd1cdf6555d6f39c4f677e28e
tjweldon <tomd90@gmail.com>
2022-05-25 00:35:16 +0100

    sketched out working server prototype

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"tjweldon/oim8/src/repl"
 )
 
 func main() {
-	DoRepl()
+	RunServer()
+	//DoRepl()
 }
 
 func DoRepl() {
@@ -28,4 +30,69 @@ func DoRepl() {
 		// exit code 1 and error message
 		log.Fatalf("oim8! FATAL: %s\n", err)
 	}
+}
+
+func RunServer() {
+	// supply the server with the stdout.
+	writer := bufio.NewWriter(os.Stdout)
+
+	// initialise the server
+	server, err := NewServer(writer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(server.Serve())
+}
+
+// Server manages the receipt of messages
+type Server struct {
+	writer *bufio.Writer
+	conn   net.PacketConn
+}
+
+// NewServer initialises the server with a PacketConn and the bufio.Writer provided
+func NewServer(writer *bufio.Writer) (*Server, error) {
+	pc, err := net.ListenPacket("udp", ":42069")
+	if err != nil {
+		return nil, err
+	}
+	s := &Server{
+		writer: writer,
+		conn:   pc,
+	}
+
+	return s, nil
+}
+
+// Close closes the underlying net.PacketConn
+func (s *Server) Close() {
+	s.conn.Close()
+}
+
+func (s *Server) Serve() error {
+	// main loop
+	for {
+		// read incoming messages
+		buf := make([]byte, 1024)
+		n, addr, err := s.conn.ReadFrom(buf)
+		if err != nil {
+			return err
+		}
+
+		// this gorutine is the worker that serves incoming messages.
+		go s.handleMsg(addr, buf[:n])
+	}
+}
+
+// handleMsg handles an incoming message
+func (s *Server) handleMsg(addr net.Addr, buf []byte) {
+	s.conn.WriteTo(buf, addr)
+    
+    _, err := s.writer.Write(buf)
+    if err != nil {
+        fmt.Printf("Encountered error [%s] wile handling message: %s\n", err, buf)
+        return
+
+    s.writer.Flush()
 }
